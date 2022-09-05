1c6ce4e959923f6dd47c319cd0177c9c36fc3e67
LukeSaundersCOLC <108867580+LukeSaundersCOLC@users.noreply.github.com>
2022-09-05 09:06:51 +0100

    Version 0
    Connected to ProSolution & ProMonitor

---
+++ ExamAccessArrangements/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamAccessArrangements.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
