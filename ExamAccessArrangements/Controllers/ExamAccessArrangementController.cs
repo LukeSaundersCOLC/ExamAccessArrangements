8a7455a78053a68da8e15cc7387c1caaf85b517b
LukeSaundersCOLC <108867580+LukeSaundersCOLC@users.noreply.github.com>
2022-09-06 12:53:47 +0100

    Amended Parameters

--- ExamAccessArrangements/Controllers/ExamAccessArrangementController.cs
+++ ExamAccessArrangements/Controllers/ExamAccessArrangementController.cs
@@ -6,12 +6,12 @@ namespace ExamAccessArrangements.Controllers
     public class ExamAccessArrangementsController : Controller
     {
         private readonly ProSolutionContext _pscontext;
-        private readonly string sqlStudentInfo = "eea.spEAAStudentInfo_20220905LS";
-        private readonly string sqlEnrolmentInfo = "eea.spEAAEnrolmentInfo_20220905LS";
+        private readonly string sqlStudentInfo = "eea.spEAAStudentInfo_20220905LS @AY, @RefNo";
+        private readonly string sqlEnrolmentInfo = "eea.spEAAEnrolmentInfo_20220905LS @AY, @RefNo";
         private readonly string sqlTimetableInfo = "eea.spEAATimetableAndRegisterInfo_20220905LS";
-        private readonly string sqlpsALSInfo = "eea.spEAApsALSInfo_20220905LS";
+        private readonly string sqlpsALSInfo = "eea.spEAApsALSInfo_20220905LS @AY, @RefNo";
         private readonly ProMonitorContext _pmcontext;
-        private readonly string sqlpmALSInfo = "eea.spEAApmALSInfo_20220905LS";
+        private readonly string sqlpmALSInfo = "eea.spEAApmALSInfo_20220905LS @AY, @RefNo";
         public ExamAccessArrangementsController(ProSolutionContext pscontext, string sqlStudentInfo, string sqlEnrolmentInfo, string sqlTimetableInfo, string sqlALSInfo, ProMonitorContext pmcontext, string sqlpmALSInfo)
         {
             _pscontext = pscontext;
--- ExamAccessArrangements/Data/ProSolutionContext.cs
+++ ExamAccessArrangements/Data/ProSolutionContext.cs
@@ -13,13 +13,6 @@ namespace GroupProfileSummary.Data
         public virtual DbSet<EnrolmentInfo> Enrolment { get; set; } = null!;
         public virtual DbSet<TimetableInfo> Timetables { get; set; } = null!;
         public virtual DbSet<PSALSInfo> PSALS { get; set; } = null!;
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //                optionsBuilder.UseSqlServer("Server=warehouse;Database=WebApplications;Trusted_Connection=True;");
-            }
-        }
+        
     }
 }
