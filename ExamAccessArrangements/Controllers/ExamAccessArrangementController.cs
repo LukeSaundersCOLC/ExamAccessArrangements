f84139a46d4a7c408a860286c6da8a848b725ed4
LukeSaundersCOLC <108867580+LukeSaundersCOLC@users.noreply.github.com>
2022-09-07 13:05:31 +0100

    Update ExamAccessArrangementController.cs

--- ExamAccessArrangements/Controllers/ExamAccessArrangementController.cs
+++ ExamAccessArrangements/Controllers/ExamAccessArrangementController.cs
@@ -8,8 +8,8 @@ namespace ExamAccessArrangements.Controllers
     public class ExamAccessArrangementsController : Controller
     {
         private readonly ProSolutionContext _pscontext;
-        private readonly string sqlStudentInfo = "eea.spEAAStudentInfo_20220905LS @AY, @RefNo, @SDiD";
-        private readonly string sqlEnrolmentInfo = "eea.spEAAEnrolmentInfo_20220905LS @AY, @RefNo, @SDiD";
+        private readonly string sqlStudentInfo = "eea.spEAAStudentInfo_20220905LS @AY, @RefNo, @SD";
+        private readonly string sqlEnrolmentInfo = "eea.spEAAEnrolmentInfo_20220905LS @AY, @RefNo, @SD";
         private readonly string sqlTimetableInfo = "eea.spEAATimetableAndRegisterInfo_20220905LS";
         private readonly string sqlpsALSInfo = "eea.spEAApsALSInfo_20220905LS @AY, @RefNo";
         private readonly ProMonitorContext _pmcontext;
@@ -24,12 +24,12 @@ namespace ExamAccessArrangements.Controllers
             _pmcontext = pmcontext;
             this.sqlpmALSInfo = sqlpmALSInfo;
         }
-        public IActionResult Index(string? AY, string? RefNo, string? SDiD)
+        public IActionResult Index(string? AY, string? RefNo, string? SD)
         {
             ViewBag.AY = AY;
             SqlParameter p1 = new("@AY", AY);
             SqlParameter p2 = new("@RefNo", RefNo);
-            SqlParameter p3 = new("@SDiD", SDiD);
+            SqlParameter p3 = new("@SD", SD);
            
             var s = _pscontext.Student.FromSqlRaw(sqlStudentInfo, p1, p2,p3).ToList();
             var e = _pscontext.Enrolment.FromSqlRaw(sqlEnrolmentInfo, p1, p2,p3).ToList();
